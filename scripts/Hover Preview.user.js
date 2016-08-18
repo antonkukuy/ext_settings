b1dab6fd6d516793225b3c21952026faad3d024e
Anton <antonkukuy@mail.ru>
2016-08-18 15:07:23 +0800

    tabs changed spaces

--- scripts/Hover Preview.user.js
+++ scripts/Hover Preview.user.js
@@ -25,7 +25,7 @@
 // StackOverflow links!
 
 // if (window.document != document) {
-	// return; // Don't run in iframes
+    // return; // Don't run in iframes
 // }
 
 // Quite nice on apache file listings of .jpegs, but a bit slow.  Ideally pre-load hoverable images?
@@ -45,114 +45,114 @@ var myFrame;
 var isOverPopup = false;
 
 function checkFocus() {
-	if (focus) {
-		// if (focus == lastFocus) {
-			// User has definitely been here a while
-			showPreviewWindow(focus);
-		// } else {
-		// }
-		// lastFocus = focus;
-	}
+    if (focus) {
+        // if (focus == lastFocus) {
+            // User has definitely been here a while
+            showPreviewWindow(focus);
+        // } else {
+        // }
+        // lastFocus = focus;
+    }
 }
 
 function eekAMouse(evt) {
-	if (evt.currentTarget.tagName !== "A") {
-		return;
-	}
-	if (!focus && evt.altKey) {
-		focus = evt.currentTarget;
-		// setTimeout('checkFocus();',focusReactionTime);
-		// Hack to bring the popup back immediately if we've gone back to the same link.
-		if (myFrame && focus.href && myFrame.href == focus.href) {
-			showPreviewWindow(focus,evt);
-		} else {
-			if (timer) {
-				clearTimeout(timer);
-			}
-			timer = setTimeout(checkFocus,focusReactionTime);
-		}
-	} else {
-		window.status = "Already focused on a link wtf!";
-	}
+    if (evt.currentTarget.tagName !== "A") {
+        return;
+    }
+    if (!focus && evt.altKey) {
+        focus = evt.currentTarget;
+        // setTimeout('checkFocus();',focusReactionTime);
+        // Hack to bring the popup back immediately if we've gone back to the same link.
+        if (myFrame && focus.href && myFrame.href == focus.href) {
+            showPreviewWindow(focus,evt);
+        } else {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(checkFocus,focusReactionTime);
+        }
+    } else {
+        window.status = "Already focused on a link wtf!";
+    }
 }
 
 function phewMouseGone(evt) {
-	if (evt.currentTarget.tagName !== "A") {
-		return;
-	}
-	focus = undefined;
-	if (timer) {
-		clearTimeout(timer);
-	}
-	// TESTING: Don't hide the popup if mouse is currently over the popup!
-	timer = setTimeout(clearPopup,unfocusReactionTime);
+    if (evt.currentTarget.tagName !== "A") {
+        return;
+    }
+    focus = undefined;
+    if (timer) {
+        clearTimeout(timer);
+    }
+    // TESTING: Don't hide the popup if mouse is currently over the popup!
+    timer = setTimeout(clearPopup,unfocusReactionTime);
 }
 
 function clearPopup(e) {
-	if (isOverPopup || focus)
-		return;
-	if (myPopup) {
-		// myPopup.parentNode.removeChild(myPopup);
-		// myPopup = undefined; // eww cache it!
-		myPopup.style.display = 'none';
-	}
+    if (isOverPopup || focus)
+        return;
+    if (myPopup) {
+        // myPopup.parentNode.removeChild(myPopup);
+        // myPopup = undefined; // eww cache it!
+        myPopup.style.display = 'none';
+    }
 }
 
 // DONE: If the user clicks a link, this isn't really a hover, so we should not
 // activate and just let the user's click be processed!
 function aClick(evt) {
-	focus = undefined;
+    focus = undefined;
 }
 
 function createPopup() {
-	// Create frame
-	myPopup = document.createElement('DIV');
-	/** Seems style does not work for Konqueror this way. **/
-	myPopup.innerHTML =
-		"<STYLE type='text/css'> iframe.preview { color: #ff8822; background-color: #fff; margin: 0px; padding: 2px; border: 2px solid red; text-align: center; } </STYLE>"
-		+
-		"<IFRAME class='preview' width='"+(window.innerWidth*0.8)+"' height='"+(window.innerHeight*0.9)+"' src='about:blank'></IFRAME>";
-	myPopup.addEventListener("mouseover", function(evt) { isOverPopup=true; }, false);
-	myPopup.addEventListener("mouseout", function(evt) { isOverPopup=false; setTimeout(clearPopup,unfocusReactionTime); }, false);
+    // Create frame
+    myPopup = document.createElement('DIV');
+    /** Seems style does not work for Konqueror this way. **/
+    myPopup.innerHTML =
+        "<STYLE type='text/css'> iframe.preview { color: #ff8822; background-color: #fff; margin: 0px; padding: 2px; border: 2px solid red; text-align: center; } </STYLE>"
+        +
+        "<IFRAME class='preview' width='"+(window.innerWidth*0.8)+"' height='"+(window.innerHeight*0.9)+"' src='about:blank'></IFRAME>";
+    myPopup.addEventListener("mouseover", function(evt) { isOverPopup=true; }, false);
+    myPopup.addEventListener("mouseout", function(evt) { isOverPopup=false; setTimeout(clearPopup,unfocusReactionTime); }, false);
     window.addEventListener("keyup", function(evt) { if(evt.keyCode == 27){ isOverPopup=false; setTimeout(clearPopup,unfocusReactionTime); } }, false);
 
-	document.documentElement.appendChild(myPopup);
-	/*
-	myPopup.style.border = "4px solid white";
-	myPopup.style.backgroundColor = "#004400";
-	myPopup.style.margin = "4px";
-	myPopup.style.padding = "4px";
-	*/
-	//myPopup.style.width = window.innerWidth * 0.9 + "px";
-	//myPopup.style.height = window.innerHeight * 0.9 + "px";
-	myPopup.style.position = "fixed";
-	myPopup.style.right = "12px";
-	myPopup.style.bottom = "12px";
-	myPopup.style.zIndex = "10000";
-	myFrame = myPopup.getElementsByTagName('IFRAME')[0];
+    document.documentElement.appendChild(myPopup);
+    /*
+    myPopup.style.border = "4px solid white";
+    myPopup.style.backgroundColor = "#004400";
+    myPopup.style.margin = "4px";
+    myPopup.style.padding = "4px";
+    */
+    //myPopup.style.width = window.innerWidth * 0.9 + "px";
+    //myPopup.style.height = window.innerHeight * 0.9 + "px";
+    myPopup.style.position = "fixed";
+    myPopup.style.right = "12px";
+    myPopup.style.bottom = "12px";
+    myPopup.style.zIndex = "10000";
+    myFrame = myPopup.getElementsByTagName('IFRAME')[0];
 }
 
 function showPreviewWindow(link,evt) {
-	if (!myFrame) {
-		createPopup();
-	}
-	myPopup.style.display = '';
-	if (!myFrame.src || myFrame.src != link.href)
-		myFrame.src = link.href;
+    if (!myFrame) {
+        createPopup();
+    }
+    myPopup.style.display = '';
+    if (!myFrame.src || myFrame.src != link.href)
+        myFrame.src = link.href;
 }
 
 function init() {
-	for (var i=0;i<document.links.length;i++) {
-		var link = document.links[i];
-		/** Apparently deprecated. **/
-		// link.onmouseover = eekAMouse;
-		// link.onmouseout = phewMouseGone;
-		/** The new way: **/
-		link.addEventListener("mouseover", eekAMouse, false);
-		link.addEventListener("mouseout", phewMouseGone, false);
-		link.addEventListener("click", aClick, false);
-		// link.addEventListener("mousemove", function(evt) { locate(evt); }, true);
-	}
+    for (var i=0;i<document.links.length;i++) {
+        var link = document.links[i];
+        /** Apparently deprecated. **/
+        // link.onmouseover = eekAMouse;
+        // link.onmouseout = phewMouseGone;
+        /** The new way: **/
+        link.addEventListener("mouseover", eekAMouse, false);
+        link.addEventListener("mouseout", phewMouseGone, false);
+        link.addEventListener("click", aClick, false);
+        // link.addEventListener("mousemove", function(evt) { locate(evt); }, true);
+    }
 }
 
 init();
