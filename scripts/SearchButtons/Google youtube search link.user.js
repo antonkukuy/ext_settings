8020eeceb49223725cfbea0e9f5ef6fabe1774cb
Anton <antonkukuy@mail.ru>
2017-11-09 17:51:45 +0800

    nodes added to google search panel

--- scripts/SearchButtons/Google youtube search link.user.js
+++ scripts/SearchButtons/Google youtube search link.user.js
@@ -18,6 +18,24 @@ new MutationObserver(process).observe(document, {childList:true, subtree:true});
 
 function process(mutations) {
 	var youtube = document.getElementById('__YOUTUBE_SEARCH__');
+
+  var youtube = document.getElementById('__YOUTUBE_SEARCH__');
+  var amazon = document.getElementById('__AMAZON_SEARCH__');
+  var ebay = document.getElementById('__EBAY_SEARCH__');
+  var yandex = document.getElementById('__YANDEX_SEARCH__');
+  var gitbook = document.getElementById('__GITBOOK_SEARCH__');
+  var javascript = document.getElementById('__JAVASCRIPT_SEARCH__');
+  var stackoverflow = document.getElementById('__STACKOVERFLOW_SEARCH__');
+  var habrahabr = document.getElementById('__HABRAHABR_SEARCH__');
+  var mozila = document.getElementById('__MOZILA_SEARCH__');
+  var vim = document.getElementById('__VIM_SEARCH__');
+  var vimcast = document.getElementById('__VIMCAST_SEARCH__');
+  var w3schools = document.getElementById('__W3SCHOOLS_SEARCH__');
+  var bash = document.getElementById('__BASH_SEARCH__');
+  // if (state > 2)
+  // //if (state > 10)
+  //   return;
+
 	if (youtube)
 		return;
 	var menu = document.getElementById('hdtb-msb');
@@ -62,7 +80,61 @@ function process(mutations) {
 			if (youtube.previousElementSibling != node)
 				youtube.parentNode.insertBefore(youtube, node.nextElementSibling);
 		}
-	}
+
+
+      arrangeElements(amazon, youtube);
+      arrangeElements(ebay, amazon);
+      arrangeElements(yandex, ebay);
+      arrangeElements(gitbook, yandex);
+      arrangeElements(javascript, gitbook);
+      arrangeElements(stackoverflow, javascript);
+      arrangeElements(javascript, stackoverflow);
+      arrangeElements(mozila, javascript);
+      arrangeElements(habrahabr, mozila);
+      arrangeElements(vim, habrahabr);
+      arrangeElements(vimcast, vim);
+      arrangeElements(w3schools, vimcast);
+      arrangeElements(bash ,w3schools);
+  }
+
+newNodes(youtube, "__YOUTUBE_SEARCH__", "https://www.youtube.com/results?search_query=", "Youtube");
+newNodes(amazon, "__AMAZON_SEARCH__", "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=", "Amazon");
+newNodes(ebay, "__EBAY_SEARCH__", "http://www.ebay.com/sch/i.html?_nkw=", "Ebay");
+newNodes(yandex, "__YANDEX_SEARCH__", "https://yandex.ru/search/?text=", "Yandex");
+newNodes(gitbook, "__GITBOOK_SEARCH__", "https://git-scm.com/search/results?search=", "GitBook");
+newNodes(javascript, "__JAVASCRIPT_SEARCH__", "https://learn.javascript.ru/search?query=", "Learn.js");
+newNodes(stackoverflow, "__STACKOVERFLOW_SEARCH__", "http://stackoverflow.com/search?q=", "Stackoverflow");
+newNodes(habrahabr, "__HABRAHABR_SEARCH__", "https://habrahabr.ru/search/?q=", "Habrahabr");
+newNodes(mozila, "__MOZILA_SEARCH__", "https://developer.mozilla.org/en-US/search?q=", "MDN");
+newNodes(vim, "__VIM_SEARCH__", "https://www.google.ru/search?q=dogcart&gws_rd=cr&ei=oAi2V-qxM4G6sgHW7KaICw#newwindow=1&q=site:vimhelp.appspot.com+", "VIM");
+newNodes(vimcast, "__VIMCAST_SEARCH__", "http://vimcasts.org/results/#stq=", "VimCast");
+newNodes(w3schools, "__W3SCHOOLS_SEARCH__", "https://www.google.ru/search?q=dogcart&gws_rd=cr&ei=oAi2V-qxM4G6sgHW7KaICw#newwindow=1&q=site:w3schools.com+", "w3schools");
+newNodes(bash, "__BASH_SEARCH__", "https://www.google.ru/search?q=dogcart&gws_rd=cr&ei=oAi2V-qxM4G6sgHW7KaICw#newwindow=1&q=site:tldp.org/LDP/abs/html+", "Bash");
+
+function arrangeElements(element, prevElement){
+    if (element) {
+      node = prevElement || menu.lastElementChild;
+      if (element.previousElementSibling != node)
+        element.parentNode.insertBefore(element, node.nextElementSibling);
+    }
+}
+function newNodes(nameNode, nameId, queryPage, nameLink){
+
+  if (!nameNode) {
+    var q = document.querySelector('#searchform input[dir]');
+    if (q) q = encodeURIComponent(q.value);
+    else if ((q = location.href.match(/^.+?(?:[#\/&?](?:q|query))=(.+?)(?:|&.+|\|.+)$/)))
+      q = q[1];
+    else q = '';
+    node.insertAdjacentHTML(place,
+      '<div class="hdtb-mitem hdtb-imb" id="' + nameId + '">\
+         <a class="q qs" href="' + queryPage + q + '" >' + nameLink + '</a>\
+      </div>');
+  }
+  // state++;
+}
+
+
 
 	if (!youtube) {
 		var q = '', queryElement = document.querySelector('input[name="q"]');
